14cf6ede629e5e772109e34bb82b849ac2173ae0
Yehison Perdomo <yehison93@gmail.com>
2024-05-16 18:48:43 -0400

    prueba 02

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,24 +1,22 @@
-import "aframe";
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
 
-const PlayerVr = () => {
+function App() {
   return (
-    <a-scene>
-      <a-assets>
-        <video
-          id="videoiptv"
-          autoPlay
-          src="https://vcp2.myplaytv.com/valetv/valetv/playlist.m3u8"
-        ></video>
-      </a-assets>
-
-      <a-videosphere
-        src="#videoiptv"
-        width="16"
-        height="9"
-        position="0 0 -20"
-      ></a-videosphere>
-    </a-scene>
+    <>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
   );
-};
+}
 
-export default PlayerVr;
+export default App;
