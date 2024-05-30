ea27e5a38d5bf4c4b81f6ffb39fbf25a3ad729d5
Yehison Perdomo <yehison93@gmail.com>
2024-05-29 23:04:00 -0400

    prueba de video

--- src/components/player/customHooks/useHlsVideo.jsx
+++ src/components/player/customHooks/useHlsVideo.jsx
@@ -8,16 +8,23 @@ const useHlsVideo = ({ url }) => {
 
   const playVideo = () => {
     const configHls = {
-      minAutoBitrate: 0,
+      minAutoBitrate: 100000, 
       lowLatencyMode: true,
-      debug: true,
+      startPosition: 0, 
+      maxBufferLength: 30, 
+      maxMaxBufferLength: 30,
     };
     if (Hls.isSupported()) {
       const hls = new Hls(configHls);
       hls.loadSource(url);
       hls.attachMedia(videoRef.current);
+      hls.recoverMediaError(videoRef.current);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
         videoRef.current.play();
+      });
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        console.log("error hls: ", event, data);
+        hls.recoverMediaError();
       });
     } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
       videoRef.current.src = url;
