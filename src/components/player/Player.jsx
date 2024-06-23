a67584494bb2435db1afe04ef18eaf99ee5b818f
Yehison Perdomo <yehison93@gmail.com>
2024-06-23 04:20:38 -0400

    ajuste del video

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -32,109 +32,112 @@ const Player = ({
       thetaLength: getThetaLength(settings.sizeTV),
     }));
   }, [settings.sizeTV]);
+
   return (
-    <Scene
-      id="MainScene"
-      className="container-player"
-      style={{ with: "100vw", height: "100vh", margin: "0", padding: "0" }}
-      device-orientation-permission-ui="
+    <>
+      <video
+        id="videoassets"
+        controls={true}
+        src={null}
+        preload="auto"
+        ref={playerRef}
+        autoPlay={true}
+        crossOrigin={"anonymous"}
+        muted={!settings.stateVideo}
+        width={16}
+        height={9}
+        playsInline={true}
+      />
+      <Scene
+        id="MainScene"
+        className="container-player"
+        style={{ with: "100vw", height: "100vh", margin: "0", padding: "0" }}
+        device-orientation-permission-ui="
           enabled: true;
           denyButtonText: Denegar;
           allowButtonText: Permitir;
           deviceMotionMessage: Esta es una app de VR que requiere de tu permiso para acceder a los sensores de movimiento de tu dispositivo, deberías aceptar para que tengas la mejor experiencia posible.;
         "
-      loading-screen="dotsColor: darkblue; backgroundColor: aqua"
-      renderer="
+        loading-screen="dotsColor: darkblue; backgroundColor: aqua"
+        renderer="
           highRefreshRate: true;
           foveationLevel: 1;
           multiviewStereo: true;
           precision: medium;
         "
-      fog={`
+        fog={`
         type: exponential; color: ${
           settings.stateVideo ? settings.fog.color : "white"
         };
         density: ${settings.stateVideo ? settings.fog.density : 0.015}
         `}
-      effect={true}
-      xr-mode-ui={`enabled: true; enterVREnabled: true; enterVRButton: #myEnterVRButton; cardboardModeEnabled: true`}
-      events={{
-        "enter-vr": () => {
-          setSettings((prevSettings) => ({
-            ...prevSettings,
-            modeVR: true,
-          }));
-        },
-        "exit-vr": () => {
-          setSettings((prevSettings) => ({
-            ...prevSettings,
-            timeTherapy: null,
-            rotation: "0 0 0",
-            modeVR: false,
-          }));
-          finishTime();
-        },
-      }}
-    >
-      <Entity
+        effect={true}
+        xr-mode-ui={`enabled: true; enterVREnabled: true; enterVRButton: #myEnterVRButton; cardboardModeEnabled: true`}
         events={{
-          loaded: () => {
+          "enter-vr": () => {
             setSettings((prevSettings) => ({
               ...prevSettings,
-              loadedVR: true,
+              modeVR: true,
             }));
           },
+          "exit-vr": () => {
+            setSettings((prevSettings) => ({
+              ...prevSettings,
+              timeTherapy: null,
+              rotation: "0 0 0",
+              modeVR: false,
+            }));
+            finishTime();
+          },
         }}
-        primitive="a-assets"
-        timeout="10000"
       >
-        <audio
-          ref={audioRef}
-          autoPlay={true}
-          src={null}
-          muted={!settings.stateVideo}
-          crossOrigin={"anonymous"}
-          preload="metadata"
-          controls={true}
-        />
-        <video
-          id="videoassets"
-          controls={true}
-          src={null}
-          preload="auto"
-          ref={playerRef}
-          autoPlay={true}
-          crossOrigin={"anonymous"}
-          muted={!settings.stateVideo}
-          width={16}
-          height={9}
-          playsInline={true}
-        />
-        <img id="logo" src={settings.logo} loading="eager" />
-        <img id="sky" src={settings.backGround} loading="eager" />
-        <img id="playIcon" src={playIcon} width={100} height={100} />
-        <img id="pauseIcon" src={pauseIcon} width={100} height={100} />
-        <img id="chevronIcon" src={chevronIcon} width={100} height={100} />
-      </Entity>
-
-      <Entity light="type: ambient; color: #ffd28e" />
-      <Logo settings={settings} />
-      <Camera settings={settings} />
-      <BackGround settings={settings} />
-      <Patch settings={settings} />
-      <Entity position={`0 ${settings.positionTV} 0`} rotation="15 0 0">
-        <Tv settings={settings} playerRef={playerRef} param={param} />
-        {settings.modeVR ? (
-          <Controls
-            playerRef={playerRef}
-            attachVideo={attachVideo}
-            changeChannels={changeChannels}
-            settings={settings}
-            param={param}
+        <Entity
+          events={{
+            loaded: () => {
+              setSettings((prevSettings) => ({
+                ...prevSettings,
+                loadedVR: true,
+              }));
+            },
+          }}
+          primitive="a-assets"
+          timeout="10000"
+        >
+          <audio
+            ref={audioRef}
+            autoPlay={true}
+            src={null}
+            muted={!settings.stateVideo}
+            crossOrigin={"anonymous"}
+            preload="metadata"
+            controls={true}
           />
-        ) : null}
-      </Entity>
-    </Scene>
+          <img id="logo" src={settings.logo} loading="eager" />
+          <img id="sky" src={settings.backGround} loading="eager" />
+          <img id="playIcon" src={playIcon} width={100} height={100} />
+          <img id="pauseIcon" src={pauseIcon} width={100} height={100} />
+          <img id="chevronIcon" src={chevronIcon} width={100} height={100} />
+        </Entity>
+
+        <Entity light="type: ambient; color: #ffd28e" />
+        <Logo settings={settings} />
+        <Camera settings={settings} />
+        <BackGround settings={settings} />
+        <Patch settings={settings} />
+        <Entity position={`0 ${settings.positionTV} 0`} rotation="15 0 0">
+          <Tv settings={settings} playerRef={playerRef} param={param} />
+          {settings.modeVR ? (
+            <Controls
+              playerRef={playerRef}
+              attachVideo={attachVideo}
+              changeChannels={changeChannels}
+              settings={settings}
+              param={param}
+            />
+          ) : null}
+        </Entity>
+      </Scene>
+    </>
   );
 };
 
